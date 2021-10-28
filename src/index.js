cc6cd42b6afd180f42dde8ea748ca3473e8c7088
Prasanna Mangoli <mangoli.prasanna05@gmail.com>
2021-10-28 12:55:06 +0530

    db init

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const app = require('express')();
 
+const { initDatabase } = require('@makosmods/sequelize-helper');
+
+const path = require('path');
+
 const init = require('@makosmods/init');
 
 const config = require('./configuration/config');
 
-init(app, __dirname, config)
+initDatabase( { ...config.database } , path.join(__dirname, 'model'))
+    .then((e) => init(app, __dirname, config))
     .then((e) => {
         console.log("Application started on port ", config.port);
+    })
+    .catch((e) => {
+        console.log("Failed to start service", e);
     });
