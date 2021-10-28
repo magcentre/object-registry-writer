894ee2422c2a05d68f2a9a5fb0603c481a951b6b
Prasanna Mangoli <mangoli.prasanna05@gmail.com>
2021-10-28 17:02:02 +0530

    swagger and null check

--- src/controllers/registry.controller.js
+++ src/controllers/registry.controller.js
@@ -3,10 +3,14 @@ const processor = require('../processors/register.process');
 const config = require('../configuration/config');
 
 const upload = (req, res) => {
+
+    if(!req.files.file) return res.status(400).json({"message": "file missing in request"});
+
     let file = {
         ...req.files.file,
         bucket: config.minio.bucket
     }
+    
     processor.uploadToMinio(file)
         .then((e) => processor.createRegistryEntry(e))
         .then((e) => res.status(200).json(e));
