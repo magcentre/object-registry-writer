f9e5a830bc6a27ef1913aeca14dd3c1f30372a54
Prasanna Mangoli <mangoli.prasanna05@gmail.com>
2021-10-28 12:55:27 +0530

    db config

--- src/configuration/config.js
+++ src/configuration/config.js
@@ -3,4 +3,11 @@ const DEFAULT_CONFIG_VERSION = 'v1.0.0';
 module.exports = {
   env: process.env.NODE_ENV ?? 'development',
   port: process.env.PORT ?? 5001,
+  database: {
+    host: process.env.dbhost,
+    port: process.env.dbport,
+    user: process.env.dbuser,
+    password: process.env.dbpassword,
+    database: process.env.dbname,
+  }
 }
