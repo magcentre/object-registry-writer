4861182eb130723ec7a1899dc2fb375ca863e99d
Prasanna Mangoli <mangoli.prasanna05@gmail.com>
2021-10-28 15:34:30 +0530

    minio init

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 const app = require('express')();
 
-const { initDatabase } = require('@makosmods/sequelize-helper');
-
 const path = require('path');
 
-const init = require('@makosmods/init');
+const initServer = require('@makosmods/init');
+
+const initMinio = require('@makosmods/minio-helper').initMinio;
+
+const { initDatabase } = require('@makosmods/sequelize-helper');
 
 const config = require('./configuration/config');
 
 initDatabase( { ...config.database } , path.join(__dirname, 'model'))
-    .then((e) => init(app, __dirname, config))
+    .then((e) => initMinio(config.minio))
+    .then((e) => initServer(app, __dirname, config))
     .then((e) => {
-        console.log("Application started on port ", config.port);
+        console.log("Service started on port ", config.port);
     })
-    .catch((e) => {
-        console.log("Failed to start service", e);
+    .catch((err) => {
+        console.log("Failed to start service", err);
     });
