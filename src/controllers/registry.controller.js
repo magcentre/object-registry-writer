3c633c97bb946eada73b5f29ed93bc412766355a
Prasanna Mangoli <Prasanna Mangoli>
2022-01-03 18:17:53 +0530

    registry create api

--- src/controllers/registry.controller.js
+++ src/controllers/registry.controller.js
@@ -1,4 +1,5 @@
 const { sendResult, sendError, getRichError } = require('@magcentre/response-helper');
+
 const processor = require('../processors/register.process');
 
 const config = require('../configuration/config');
@@ -21,6 +22,14 @@ const upload = (req, res) => {
     .catch((e) => sendError(e, res, 500, req));
 };
 
+const create = (req, res) => {
+
+  processor.createRegistryEntry({ ...req.body, accessKey: req.body.url })
+    .then((e) => sendResult(e, 200, res, req))
+    .catch((e) => sendError(e, res, 500, req));
+}
+
 module.exports = {
   upload,
+  create
 };
