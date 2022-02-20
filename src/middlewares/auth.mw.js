4d99bb43d3cb060bf2d610f9eb9366046a816a80
Prasanna Mangoli <Prasanna Mangoli>
2022-02-20 21:29:45 +0530

    jwt-auth-with-utils

--- src/middlewares/auth.mw.js
+++ src/middlewares/auth.mw.js
@@ -1,19 +1,20 @@
-const jwt = require('jsonwebtoken');
-
 const { sendError } = require('@magcentre/response-helper');
+
+const utils = require('@magcentre/api-utils');
 
 const config = require('../configuration/config');
 
-module.exports.jwt = function (req, res, next) {
-  if (!req.headers.authorization) return sendError('Authentication required', res, 401, req);
+module.exports.jwt = async (req, res, next) => {
+  if (!req.headers.authorization) return sendError({ message: 'Authentication required' }, res, 401, req);
 
-  jwt.verify(req.headers.authorization.replace('Bearer ', ''), config.jwt.secret, (error, decoded, info) => {
-    if (error) return sendError('Error in JWT authentication process', res, 500, req);
+  const token = req.headers.authorization.replace('Bearer ', '');
 
-    if (!decoded) return sendError(`Authentication failed,: ${info}`, res, 401, req);
-
+  try {
+    const decoded = await utils.verifyJWTToken(token, config.jwt.secret);
     req.auth = decoded;
-
     return next();
-  });
+  }
+  catch (err) {
+    return sendError(err, res, err.statusCode || 500, req);
+  }
 };
